86efbc7b3a7cf4445ca67abb069022ada96f7780
Archit Atrey <architatrey24@gmail.com>
2026-09-14 01:50:20 +0530

    Scaffold Spring Boot project

---
+++ src/main/java/com/example/realtimeviewercounter/RealtimeViewerCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.realtimeviewercounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeViewerCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeViewerCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/realtimeviewercounter/RealtimeViewerCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtimeviewercounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeViewerCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
