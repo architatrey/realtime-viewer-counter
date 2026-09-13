f45d3699c4c1c8885e311763b262cc2612b933a7
Archit Atrey <architatrey24@gmail.com>
2026-09-14 02:39:24 +0530

    Configure timezone to UTC globally and for Jackson

--- src/main/java/com/example/realtimeviewercounter/RealtimeViewerCounterApplication.java
+++ src/main/java/com/example/realtimeviewercounter/RealtimeViewerCounterApplication.java
@@ -2,9 +2,16 @@ package com.example.realtimeviewercounter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class RealtimeViewerCounterApplication {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RealtimeViewerCounterApplication.class, args);
